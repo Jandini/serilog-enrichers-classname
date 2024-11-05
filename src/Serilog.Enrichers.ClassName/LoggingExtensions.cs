3afeb2e5da445f01fd0340d33a0d108c239c82c4
Matt Janda <matt.janda.kingston@gmail.com>
2024-11-05 16:58:16 +0000

    Use serilog namespace to avoid excessive namespace using statements (#5)
    * Use serilog namespace to avoid excessive namespace using statements
    
    * Remove using statement from demo code

--- src/Serilog.Enrichers.ClassName/LoggingExtensions.cs
+++ src/Serilog.Enrichers.ClassName/LoggingExtensions.cs
@@ -1,27 +1,26 @@
 ﻿using Serilog.Configuration;
 using System.Diagnostics;
 
-namespace Serilog.Enrichers.ClassName
+namespace Serilog;
+
+public static class LoggingExtensions
 {
-    public static class LoggingExtensions
-    {        
-        public static LoggerConfiguration WithClassName<T>(this LoggerEnrichmentConfiguration enrich) where T : class 
-        {
-            _ = enrich ?? throw new ArgumentNullException(nameof(enrich));
+    public static LoggerConfiguration WithClassName<T>(this LoggerEnrichmentConfiguration enrich) where T : class
+    {
+        _ = enrich ?? throw new ArgumentNullException(nameof(enrich));
 
-            enrich.WithProperty("SourceContext", typeof(T).FullName);
-            return enrich.With<ClassNameEnricher>();
-        }
+        enrich.WithProperty("SourceContext", typeof(T).FullName);
+        return enrich.With<ClassNameEnricher>();
+    }
 
-        public static LoggerConfiguration WithClassName(this LoggerEnrichmentConfiguration enrich)
-        {
-            _ = enrich ?? throw new ArgumentNullException(nameof(enrich));
+    public static LoggerConfiguration WithClassName(this LoggerEnrichmentConfiguration enrich)
+    {
+        _ = enrich ?? throw new ArgumentNullException(nameof(enrich));
 
-            var stackFrame = new StackTrace().GetFrame(1);
-            var declaringType = stackFrame?.GetMethod()?.DeclaringType;
+        var stackFrame = new StackTrace().GetFrame(1);
+        var declaringType = stackFrame?.GetMethod()?.DeclaringType;
 
-            enrich.WithProperty("SourceContext", declaringType?.FullName ?? "Unknown");
-            return enrich.With<ClassNameEnricher>();
-        }
+        enrich.WithProperty("SourceContext", declaringType?.FullName ?? "Unknown");
+        return enrich.With<ClassNameEnricher>();
     }
 }
