cd33ed2e7f43800d34ec4c53d8da9f7d6dac7d60
Matt <matt.janda.kingston@gmail.com>
2024-11-05 15:55:18 +0000

    Use serilog namespace to avoid excessive namespace using statements

--- src/Serilog.Enrichers.ClassName/ClassNameEnricher.cs
+++ src/Serilog.Enrichers.ClassName/ClassNameEnricher.cs
@@ -1,21 +1,20 @@
 ﻿using Serilog.Core;
 using Serilog.Events;
 
-namespace Serilog.Enrichers.ClassName
+namespace Serilog;
+
+public class ClassNameEnricher : ILogEventEnricher
 {
-    public class ClassNameEnricher : ILogEventEnricher
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
-        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        _ = logEvent ?? throw new ArgumentNullException(nameof(logEvent));
+        _ = propertyFactory ?? throw new ArgumentNullException(nameof(propertyFactory));
+
+        if (logEvent.Properties.TryGetValue("SourceContext", out var value))
         {
-            _ = logEvent ?? throw new ArgumentNullException(nameof(logEvent));
-            _ = propertyFactory ?? throw new ArgumentNullException(nameof(propertyFactory));
+            var source = value.ToString("l", null).Split('.').Last();
 
-            if (logEvent.Properties.TryGetValue("SourceContext", out var value))
-            {
-                var source = value.ToString("l", null).Split('.').Last();
-
-                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ClassName", source));
-            }
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ClassName", source));
         }
     }
 }
