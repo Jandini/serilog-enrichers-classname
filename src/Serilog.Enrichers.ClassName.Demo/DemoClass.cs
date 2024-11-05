9bbcefbfe931033a1ac63468fc64afed3b6a530e
Matt <matt.janda.kingston@gmail.com>
2024-11-05 15:09:43 +0000

    Add demo program

---
+++ src/Serilog.Enrichers.ClassName.Demo/DemoClass.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace SerilogDemo
+{
+    internal class DemoClass
+    {
+        private static readonly ILogger Log = Serilog.Log.ForContext<DemoClass>();
+
+        public void DoWork()
+        {
+            Log.Information("Doing some work...");
+        }
+    }
+}
